dae5299ec37766e49c5dbd38754ec0bdd08871a3
YifanC <cyifan@slac.stanford.edu>
2022-10-11 11:50:32 -0700

    updated the setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 import setuptools
 
-VER = "0.0.1"
+VER = "0.0.2"
 
 setuptools.setup(
     name="LarpixParser",
     version=VER,
     author="Yifan C. and others",
-    author_email="cyifan@slac",
+    author_email="cyifan@slac.stanford.edu",
     description="A package parsing the larpix output to hit-level",
     url="https://github.com/YifanC/larpix_readout_parser",
     packages=setuptools.find_packages(where="src"), #"where" is needed; "include=['LarpixParser']" is not necessary 
@@ -16,10 +16,11 @@ setuptools.setup(
     install_requires=["numpy", "h5py", "fire"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: by End-User Class :: Developers",
-        "Operating System :: Grouping and Descriptive Categories :: OS Independent (Written in an interpreted language)",
-        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Physics"
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.2',
 )
