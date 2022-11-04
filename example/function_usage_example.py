0294eb8f508ff95ab3aef5c00f4d58658dc61584
YifanC <cyifan@slac.stanford.edu>
2022-11-03 22:20:52 -0700

    fix the t0 mess in the example use

--- example/function_usage_example.py
+++ example/function_usage_example.py
@@ -6,25 +6,31 @@ from LarpixParser import hit_parser as HitParser
 from LarpixParser import geom_dict_loader as DictLoader
 from LarpixParser import util as util
 
-f = h5py.File('../example_data/larnd-sim_output_mpr_mu100.h5', 'r')
-tracks = f['tracks']
-packets = f['packets']
-assn = f['mc_packets_assn']
+f = h5py.File('../example_data/ndlar_ev20_larndsim.h5', 'r')
+packets = f['packets'] # readout
+tracks = f['tracks'] # Geant4 truth
+assn = f['mc_packets_assn'] # G4-readout association
 
-geom_dict = DictLoader.load_geom_dict("../config_repo/dict_repo/multi_tile_layout-2.3.16.pkl")
-run_config_path = "../config_repo/module0.yaml"
+geom_dict = DictLoader.load_geom_dict("../config_repo/dict_repo/multi_tile_layout-3.0.40.pkl")
+run_config_path = "../config_repo/ndlar-module.yaml"
 run_config = util.get_run_config(run_config_path)
 
-event_ids = np.unique(tracks['eventID'])
+#########
+## The following should be the read-out level information
+## Since this package does not provide event parser, here in the example we use the truth information
+#########
+
+#event_ids = np.unique(tracks['eventID']) # not necessarily all the G4 events have readout info
 pckt_event_ids = EvtParser.packet_to_eventid(assn, tracks)
+event_ids = np.unique(pckt_event_ids[pckt_event_ids != -1]) 
 t0_grp = EvtParser.get_t0(packets)
 
-for evt_id in event_ids:
+for i_ev, evt_id in enumerate(event_ids):
 
     print("--------evt_id: ", evt_id)
     pckt_mask = pckt_event_ids == evt_id
     packets_ev = packets[pckt_mask]
-    t0 = t0_grp[evt_id][0]
+    t0 = t0_grp[i_ev][0]
     
     ## x is the drift; y is the vertical direction; z is horizontal axis in pixel plane
 
